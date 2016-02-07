265f26eb163b4e7321d3071c5264f1ed3eb4f722
Sampsa Sohlman <sampsa@sohlman.com>
2016-02-07 14:06:54 +0200

    add support to VaadinWebResources

--- org.liferay.vaadin7.chat.portlet/src/main/java/org/liferay/vaadin7/chat/portlet/Portlet.java
+++ org.liferay.vaadin7.chat.portlet/src/main/java/org/liferay/vaadin7/chat/portlet/Portlet.java
@@ -1,8 +1,10 @@
 package org.liferay.vaadin7.chat.portlet;
 
+import org.liferay.vaadin7.compatibilitypack.webresources.VaadinWebResource;
 import org.osgi.service.component.annotations.Component;
 
 import com.vaadin.server.VaadinPortlet;
+import com.vaadin.shared.Version;
 
 
 @Component(
@@ -12,6 +14,7 @@ import com.vaadin.server.VaadinPortlet;
 			"com.liferay.portlet.instanceable=true",
 			"javax.portlet.display-name=Vaadin Chat Portlet",
 			"javax.portlet.init-param.UI=org.liferay.vaadin7.chat.ui.PortletUI",
+			VaadinWebResource.JAVAX_PORTLET_RESOURCES_PATH,
 			"javax.portlet.security-role-ref=power-user,user"
 		},
 		service = javax.portlet.Portlet.class
